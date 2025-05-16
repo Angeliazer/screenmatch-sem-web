2d56d513e220ed94dc76e3ff555ad1f57f15fdb9
Angeliázer Rigon da Silva <yyrigon@hotmail.com>
2025-05-16 13:58:23 -0300

    Inicio projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String gravarDados(Class classe) {
+
+        try {
+            return mapper.writeValueAsString(classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
