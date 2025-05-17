15745520c922d6f837864285652db6df6e1de23f
Angeliázer Rigon da Silva <yyrigon@hotmail.com>
2025-05-17 10:06:50 -0300

    Atualizacao 2

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,9 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,24 +14,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=97e5b0ba");
-		var conversor = new ConverteDados();
 
-		var dadosSerie = conversor.getData(json, DadosSerie.class);
-		System.out.println(dadosSerie);
-
-		var dadosS = new DadosSerie(dadosSerie.titulo(), dadosSerie.totalTemporadas(), dadosSerie.avaliacao());
-
-		var convert = conversor.saveData(dadosS);
-
-		System.out.println(convert);
-
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=97e5b0ba");
-
-		var dadosEpisodio = conversor.getData(json, DadosEpisodio.class);
-
-		System.out.println(dadosEpisodio);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
 	}
 }
